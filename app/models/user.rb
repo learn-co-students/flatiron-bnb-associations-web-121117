da91fb715e5f9eef9b2f3bc1bb444aff4012ac88
github username <github email address>
2018-01-18 15:08:03 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, through: :listings, :foreign_key => 'host_id'
+  has_many :trips, class_name: 'Reservation', foreign_key: 'guest_id'
+  has_many :reviews, foreign_key: 'guest_id'
 
-  def trips
-    Reservation.all
-  end
-
-  def reviews
-    Review.all
-  end
 end
